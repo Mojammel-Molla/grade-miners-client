1a4d0d80b70e0c684868d12eefff404607af9644
Mojammel Molla <Mojammelmolla897@gmail.com>
2023-11-09 18:30:28 +0600

    Lottie animation added

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,13 +1,22 @@
 import { useContext } from 'react';
 import { AuthContext } from '../provider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
-
+import { Player } from '@lottiefiles/react-lottie-player';
 const PrivateRoute = ({ children }) => {
   const { user, loader } = useContext(AuthContext);
   const location = useLocation();
   console.log(location.pathname);
   if (loader) {
-    return <span className="loading loading-spinner text-accent"></span>;
+    return (
+      <div>
+        <Player
+          autoplay
+          loop
+          src="https://assets3.lottiefiles.com/packages/lf20_UJNc2t.json"
+          style={{ height: '300px', width: '300px' }}
+        ></Player>
+      </div>
+    );
   }
   if (user) {
     return children;
